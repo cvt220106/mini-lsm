e5c5a3157b867f93f416e9507b6acab5bc5863f9
cvt220106 <1564851464@qq.com>
2024-06-23 22:40:38 +0800

    week1&day4: impl sst encoding and metadata encoding; decoding and iterator; add the block_cache

--- mini-lsm-starter/src/table/builder.rs
+++ mini-lsm-starter/src/table/builder.rs
@@ -1,12 +1,11 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
+use bytes::BufMut;
 
-use super::{BlockMeta, SsTable};
+use super::{BlockMeta, FileObject, SsTable};
+use crate::key::KeyBytes;
 use crate::{block::BlockBuilder, key::KeySlice, lsm_storage::BlockCache};
 
 /// Builds an SSTable from key-value pairs.
@@ -22,7 +21,14 @@ pub struct SsTableBuilder {
 impl SsTableBuilder {
     /// Create a builder based on target block size.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            builder: BlockBuilder::new(block_size),
+            first_key: Vec::new(),
+            last_key: Vec::new(),
+            data: Vec::new(),
+            meta: Vec::new(),
+            block_size,
+        }
     }
 
     /// Adds a key-value pair to SSTable.
@@ -30,7 +36,30 @@ impl SsTableBuilder {
     /// Note: You should split a new block when the current block is full.(`std::mem::replace` may
     /// be helpful here)
     pub fn add(&mut self, key: KeySlice, value: &[u8]) {
-        unimplemented!()
+        // when builder is full, split block
+        while !self.builder.add(key, value) {
+            self.process();
+        }
+    }
+
+    fn process(&mut self) {
+        // new builder
+        let builder = BlockBuilder::new(self.block_size);
+        let block = std::mem::replace(&mut self.builder, builder);
+
+        // update other
+        let (first_key, last_key) = block.first_and_last_key();
+        let offset = self.data.len();
+        self.data.put(block.build().encode());
+        if self.meta.is_empty() {
+            self.first_key = first_key.raw_ref().to_vec();
+        }
+        self.last_key = last_key.raw_ref().to_vec();
+        self.meta.push(BlockMeta {
+            offset,
+            first_key,
+            last_key,
+        });
     }
 
     /// Get the estimated size of the SSTable.
@@ -38,7 +67,7 @@ impl SsTableBuilder {
     /// Since the data blocks contain much more data than meta blocks, just return the size of data
     /// blocks here.
     pub fn estimated_size(&self) -> usize {
-        unimplemented!()
+        self.data.len()
     }
 
     /// Builds the SSTable and writes it to the given path. Use the `FileObject` structure to manipulate the disk objects.
@@ -48,7 +77,29 @@ impl SsTableBuilder {
         block_cache: Option<Arc<BlockCache>>,
         path: impl AsRef<Path>,
     ) -> Result<SsTable> {
-        unimplemented!()
+        // the last block in add func, we not process it!!
+        // we need process the last block data in here
+        let mut sst = self;
+        &mut sst.process();
+        let mut data = sst.data.clone();
+        let meta = sst.meta.clone();
+
+        let offset = data.len() as u32;
+        BlockMeta::encode_block_meta(meta.as_slice(), &mut data);
+        data.put_u32(offset);
+        let file = FileObject::create(path.as_ref(), data)?;
+
+        Ok(SsTable {
+            file,
+            block_meta: meta,
+            block_meta_offset: offset as usize,
+            id,
+            block_cache,
+            first_key: KeyBytes::from_bytes(sst.first_key.into()),
+            last_key: KeyBytes::from_bytes(sst.last_key.into()),
+            bloom: None,
+            max_ts: 0,
+        })
     }
 
     #[cfg(test)]
--- mini-lsm-starter/src/table/iterator.rs
+++ mini-lsm-starter/src/table/iterator.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::sync::Arc;
 
 use anyhow::Result;
@@ -18,24 +15,47 @@ pub struct SsTableIterator {
 impl SsTableIterator {
     /// Create a new iterator and seek to the first key-value pair in the first data block.
     pub fn create_and_seek_to_first(table: Arc<SsTable>) -> Result<Self> {
-        unimplemented!()
+        let block = table.read_block_cached(0)?;
+
+        Ok(Self {
+            table,
+            blk_iter: BlockIterator::create_and_seek_to_first(block),
+            blk_idx: 0,
+        })
     }
 
     /// Seek to the first key-value pair in the first data block.
     pub fn seek_to_first(&mut self) -> Result<()> {
-        unimplemented!()
+        self.blk_idx = 0;
+        let block = self.table.read_block_cached(0)?;
+        self.blk_iter = BlockIterator::create_and_seek_to_first(block);
+
+        Ok(())
     }
 
     /// Create a new iterator and seek to the first key-value pair which >= `key`.
     pub fn create_and_seek_to_key(table: Arc<SsTable>, key: KeySlice) -> Result<Self> {
-        unimplemented!()
+        let mut sst_iter = Self::create_and_seek_to_first(table)?;
+        sst_iter.seek_to_key(key)?;
+
+        Ok(sst_iter)
     }
 
     /// Seek to the first key-value pair which >= `key`.
     /// Note: You probably want to review the handout for detailed explanation when implementing
     /// this function.
     pub fn seek_to_key(&mut self, key: KeySlice) -> Result<()> {
-        unimplemented!()
+        self.blk_idx = self.table.find_block_idx(key);
+        self.blk_iter =
+            BlockIterator::create_and_seek_to_key(self.table.read_block_cached(self.blk_idx)?, key);
+        while self.key() < key {
+            self.next()?;
+            if !self.is_valid() {
+                break;
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -44,22 +64,33 @@ impl StorageIterator for SsTableIterator {
 
     /// Return the `key` that's held by the underlying block iterator.
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.blk_iter.key()
     }
 
     /// Return the `value` that's held by the underlying block iterator.
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.blk_iter.value()
     }
 
     /// Return whether the current block iterator is valid or not.
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.blk_iter.is_valid()
     }
 
     /// Move to the next `key` in the block.
     /// Note: You may want to check if the current block iterator is valid after the move.
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        self.blk_iter.next();
+        if !self.is_valid() {
+            // move to next block
+            self.blk_idx += 1;
+            if self.blk_idx < self.table.num_of_blocks() {
+                self.blk_iter = BlockIterator::create_and_seek_to_first(
+                    self.table.read_block_cached(self.blk_idx)?,
+                );
+            }
+        }
+
+        Ok(())
     }
 }
